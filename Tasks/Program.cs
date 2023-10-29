9b05ae92f02a7cb1a054e0445279651e8b50d0df
DaniilSkv <danichskv@gmail.com>
2023-10-29 19:12:32 +0500

    Task 36 is complete

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -1,8 +1,6 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
-using System.Security.Cryptography;
-
 int[] Create_Array(int length)
 {
     int[] array = new int [length];
@@ -25,19 +23,29 @@ void CountOfEvenNums(int[] array)
     }
 }
 
-Console.Write("Привет, я создаю массив с X трехзначными положительными числами и вывожу количество чётных чисел в массиве. Введи размер массива: ");
+Console.Write("Привет, я создаю массив с X трехзначными положительными числами и вывожу количество чётных чисел в массиве, сумму элементов, стоящих на нечётных позициях. Введи размер массива: ");
 int start_num = Convert.ToInt32(Console.ReadLine());
+
+int SumNotEvenNums = 0;
 
 int[] array_fin = Create_Array(start_num);
 CountOfEvenNums(array_fin);
-Console.Write($" -> {evennums}");
+Console.WriteLine($" -> {evennums};");
+SumOfNotEvenPositionNums(array_fin);
+Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях -> {SumNotEvenNums}");
 
 // Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
-
-
+void SumOfNotEvenPositionNums(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if ((i%2) != 0)
+        SumNotEvenNums += array[i];
+    }
+}
 // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 // [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
 
